0a217ee46680fc7fd48d42c01e4011ab1689359d
Kevin Ciceri <78265431+CombativeUnicorn@users.noreply.github.com>
2023-07-27 01:03:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[20]:
-
-
 from qiskit import QuantumCircuit
-
-
-# In[21]:
+from qiskit import Aer, transpile
+from qiskit.visualization import plot_histogram
 
 
 qc = QuantumCircuit(6, 3)
+
+
+# here you have to initialize a superposition of states, in this case it is a superposition of all numbers from 0 to 7
+# please note tath this code will not work with some initial superpositions, specificly those where exactly half the numbers are multiples of 3
 qc.h([0, 1, 2])
 
 
-# In[22]:
-
-
 qc.x(3)
-
-
 qc.cx(0, 1)
 qc.x(1)
 qc.cx(2, 1)
@@ -29,16 +21,21 @@ qc.ccx(0, 2, 3)
 qc.cx(2, 1)
 qc.x(1)
 qc.cx(0, 1)
+
+
+# here you have to apply the tranformation from the initial superposition to the state of |1>
 qc.h([0, 1, 2])
 qc.x([0, 1, 2])
+
+
 qc.h(2)
 qc.ccx(0, 1, 2)
 qc.h(2)
+
+
+# here you have to apply the tranformation from the state of |1> to the initial superposition 
 qc.x([0, 1, 2])
 qc.h([0, 1, 2])
-
-
-# In[23]:
 
 
 qc.measure(0, 0)
@@ -46,20 +43,9 @@ qc.measure(1, 1)
 qc.measure(2, 2)
 
 
-# In[24]:
-
-
-from qiskit import Aer, transpile
 sim = Aer.get_backend('aer_simulator')
 t_qc = transpile(qc, sim)
 counts = sim.run(t_qc).result().get_counts()
 
-from qiskit.visualization import plot_histogram
+
 plot_histogram(counts)
-
-
-# In[ ]:
-
-
-
-
